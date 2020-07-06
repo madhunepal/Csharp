06b1e80fdac538835b11cb1f28aa761489cca3e8
madhunepal <madhunepal@gmail.com>
2020-07-05 23:43:58 -0400

    added a category class

---
+++ ECommerceCore/Model/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Model
+{
+    class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name="Category Name")]
+        public String Name { get; set; }
+
+        [Required]
+        [Display(Name = "Category Name")]
+        public int DisplayOrder { get; set; }
+    }
+}
